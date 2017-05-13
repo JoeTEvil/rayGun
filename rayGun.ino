fe6a6a0d1113f9a3f8f2acd19c1f12fc7a3def62
joeevil <joe.t.evil@gmail.com>
2017-05-13 14:00:13 +0200

    Refactoring

--- rayGun.ino
+++ rayGun.ino
@@ -17,13 +17,6 @@
 
 #define PIXEL_COUNT 17
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = pin number
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_RGB     Pixels are wired for RGB bitstream
-//   NEO_GRB     Pixels are wired for GRB bitstream, correct for neopixel stick
-//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
-//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip), correct for neopixel stick
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 bool shootStatus = false;
